efc6725ddc5d05913dbaf35e8e92b58c64f485d4
Julia Loftin <julia.loftin7@gmail.com>
2023-09-06 17:22:18 -0500

    completes task four

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,45 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job test_job1 = new Job();
+        Job test_job2 = new Job();
+        assertNotEquals(test_job1.getId(), test_job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job test_job = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(test_job.getName() instanceof String);
+        assertEquals("Product tester", test_job.getName());
+
+        assertTrue(test_job.getEmployer() instanceof Employer);
+        assertEquals("ACME", test_job.getEmployer().toString());
+
+        assertTrue(test_job.getLocation() instanceof Location);
+        assertEquals("Desert", test_job.getLocation().toString());
+
+        assertTrue(test_job.getPositionType() instanceof  PositionType);
+        assertEquals("Quality control", test_job.getPositionType().toString());
+
+        assertTrue(test_job.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", test_job.getCoreCompetency().toString());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job test_job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job test_job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(test_job1.equals(test_job2));
+    }
 }
